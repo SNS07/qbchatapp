75f5e37f3269af569c447517016f089c68579223
test <test>
2019-11-13 16:42:20 +0530

    Adding Heloku MySQl Connection

--- src/main/java/com/example/demo/repositories/ChatMessageRepositories.java
+++ src/main/java/com/example/demo/repositories/ChatMessageRepositories.java
@@ -3,9 +3,11 @@ package com.example.demo.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.UserMessages;
 
+@Repository
 public interface ChatMessageRepositories extends JpaRepository<UserMessages, Integer> {
 	
 	public List<UserMessages> findByChatRoom(String chatRoom);
